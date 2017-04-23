cdc07216ebdd411a48cd38f69961d4634302332c
VassilisPallas <vspallas@gmail.com>
2017-04-24 00:01:29 +0300

    worker queue

--- work_queues/consumer.js
+++ work_queues/consumer.js
@@ -5,12 +5,22 @@ var amqplib = require('amqplib/callback_api');
 amqplib.connect('amqp://localhost', function (err, conn) {
     conn.createChannel(function (err, ch) {
         var q = 'helloQueue';
-        ch.assertQueue(q, {durable: false});
+        ch.assertQueue(q, {durable: true});
+        // don't dispatch a new message to a worker until it has processed and acknowledged the previous one
+        ch.prefetch(1);
         ch.consume(q, function (message) {
-            var secs = message.content.toString().split('.'). length - 1;
+            /*
+             We send a message with dots, each dot is one sec.
+             We will use the timeout method to pretend we are busy in a long time
+             running task
+             */
+            var secs = message.content.toString().split('.').length - 1;
             setTimeout(function () {
                 console.log(message.content.toString());
+                ch.ack(message);
             }, secs * 1000);
-        }, {noAck: true});
+        }, {noAck: false});
+        // noAck is message acknowledgments, to turn them on we define this as false, or just skip the options
+        // if noAck is enabled, we make sure that the message is never lost if the worker dies
     });
 });
--- work_queues/sender.js
+++ work_queues/sender.js
@@ -7,6 +7,7 @@ amqplib.connect('amqp://localhost', function (err, conn) {
         var q = 'helloQueue';
         var message = process.argv.slice(2).join(' ') || 'Hello World...';
         ch.assertQueue(q, {durable: false});
-        ch.sendToQueue(q, new Buffer(message));
+        // is durable is true, when RabbitMQ server crashes or stops, the messages will stay in the queue
+        ch.sendToQueue(q, new Buffer(message), {persistent: true});
     });
 });
